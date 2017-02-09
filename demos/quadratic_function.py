d54acdd5a003c137cccc9de73f1996343a71410f
Panos Tsilifis <panostsilifis@Panoss-MacBook-Pro.local>
2017-02-08 21:24:03 -0800

    Demo.

--- demos/quadratic_function.py
+++ demos/quadratic_function.py
@@ -2,4 +2,14 @@ import numpy as np
 import scipy.stats as st 
 import matplotlib.pyplot as plt 
 import chaospy as cp
+import chaos_basispy as cb
 
+dim = 10
+np.random.seed(1234)
+W = np.random.normal(size = (dim,))
+W = W.reshape(W.shape[0],1) / np.linalg.norm(W)
+print W
+a = np.random.normal()
+b = np.random.normal()
+c = np.random.normal()
+print a,b,c
